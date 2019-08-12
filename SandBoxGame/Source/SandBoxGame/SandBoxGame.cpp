a910f307fa97e8b33db9a8d6a3b42e056656436a
GameRay <359877454@qq.com>
2019-08-12 16:36:15 +0800

    自定义样式启动卸载

--- SandBoxGame/Source/SandBoxGame/SandBoxGame.cpp
+++ SandBoxGame/Source/SandBoxGame/SandBoxGame.cpp
@@ -2,5 +2,17 @@
 
 #include "SandBoxGame.h"
 #include "Modules/ModuleManager.h"
+#include "SlateStyleRegistry.h"
+#include"SlAiStyle.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SandBoxGame, "SandBoxGame" );
+
+void SandBoxGameModule::StartupModule()
+{
+	FSlateStyleRegistry::UnRegisterSlateStyle(SlAiStyle::GetStyleSetName());
+	SlAiStyle::Initialze();
+}
+void SandBoxGameModule::ShutdownModule()
+{
+	SlAiStyle::ShutDown();
+}
--- SandBoxGame/Source/SandBoxGame/SandBoxGame.h
+++ SandBoxGame/Source/SandBoxGame/SandBoxGame.h
@@ -3,3 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+class SandBoxGameModule :public FDefaultModuleImpl
+{
+public:
+	virtual void StartupModule()override;
+	virtual void ShutdownModule()override;
+};
