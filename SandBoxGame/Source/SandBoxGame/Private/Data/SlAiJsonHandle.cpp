97a8e7b7458bcfd263487c8f73ebe50a9f64190f
GameRay <359877454@qq.com>
2019-08-24 16:21:04 +0800

    json加载解析

--- SandBoxGame/Source/SandBoxGame/Private/Data/SlAiJsonHandle.cpp
+++ SandBoxGame/Source/SandBoxGame/Private/Data/SlAiJsonHandle.cpp
@@ -4,11 +4,40 @@
 #include "SlAiJsonHandle.h"
 #include "Paths.h"
 #include "SlAiHelper.h"
+#include "Json.h"
+#include "JsonReader.h"
 
 SlAiJsonHandle::SlAiJsonHandle()
 {
 	RecordDataFileName = FString("RecordData.json");
 	RelativePath = FString("Res/ConfigData/");
+}
+
+void SlAiJsonHandle::RecordDataJsonRead(FString & Culture, float & MusicVolume, float & SoundVolume, TArray<FString>& RecordDataList)
+{
+	FString JsonValue;
+	LoadStringFromFile(RecordDataFileName, RelativePath, JsonValue);
+	TArray<TSharedPtr<FJsonValue>>JsonParsed;
+	TSharedRef<TJsonReader<TCHAR>>JsonReader = TJsonReaderFactory<TCHAR>::Create(JsonValue);
+
+	if (FJsonSerializer::Deserialize(JsonReader,JsonParsed))
+	{
+		Culture = JsonParsed[0]->AsObject()->GetStringField("Culture");
+		MusicVolume = JsonParsed[1]->AsObject()->GetNumberField("MusicVolume");
+		SoundVolume = JsonParsed[2]->AsObject()->GetNumberField("SoundVolume");
+		TArray<TSharedPtr<FJsonValue>>RecordDataArray = JsonParsed[3]->AsObject()->GetArrayField("RecordData");
+		for (int i=0;i<RecordDataArray.Num();++i)
+		{
+
+			FString RecordDataName =RecordDataArray[i]->AsObject()->GetStringField(FString::FromInt(i));
+			RecordDataList.Add(RecordDataName);
+
+		}
+	}
+	else
+	{
+		SlAiHelper::DEBUG("Deserialize Failed");
+	}
 }
 
 bool SlAiJsonHandle::LoadStringFromFile(const FString & FileName, const FString & RelaPath, FString & ResultString)
--- SandBoxGame/Source/SandBoxGame/Public/Data/SlAiJsonHandle.h
+++ SandBoxGame/Source/SandBoxGame/Public/Data/SlAiJsonHandle.h
@@ -11,6 +11,8 @@ class SANDBOXGAME_API SlAiJsonHandle
 {
 public:
 	SlAiJsonHandle();
+	//½âÎöjson
+	void RecordDataJsonRead(FString&Culture, float&MusicVolume, float&SoundVolume, TArray<FString> &RecordDataList);
 private:
 bool LoadStringFromFile(const FString&FileName,const FString &RelaPath,FString&ResultString);
 private:
