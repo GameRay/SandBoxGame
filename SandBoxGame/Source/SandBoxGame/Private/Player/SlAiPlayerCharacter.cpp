b28fafbf830c8e5a1c06c96bf930ea9ed50c511a
GameRay <359877454@qq.com>
2019-09-23 23:27:10 +0800

    初始化角色Mesh和位置

--- SandBoxGame/Source/SandBoxGame/Private/Player/SlAiPlayerCharacter.cpp
+++ SandBoxGame/Source/SandBoxGame/Private/Player/SlAiPlayerCharacter.cpp
@@ -3,13 +3,22 @@
 
 #include "SlAiPlayerCharacter.h"
 #include "ConstructorHelpers.h"
+#include "GameFramework/Character.h"
+#include "Components/SkeletalMeshComponent.h"
 // Sets default values
 ASlAiPlayerCharacter::ASlAiPlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> StaticMeshThird(TEXT("SkeletalMesh'/Game/Res/PolygonAdventure/Mannequin/Player/SkMesh/Player.Player'"));
-
+	GetMesh()->SetSkeletalMesh(StaticMeshThird.Object);
+	GetMesh()->bOnlyOwnerSee = true;
+	GetMesh()->bReceivesDecals = false;
+	GetMesh()->SetCollisionObjectType(ECC_Pawn);
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	GetMesh()->SetCollisionResponseToChannels(ECR_Ignore);
+	GetMesh()->SetRelativeLocation(FVector(0.f,0.f,-95.f));
+	GetMesh()->SetRelativeRotation(FQuat::MakeFromEuler(FVector(0.f, 0.f, - 90.f)));
 }
 
 // Called when the game starts or when spawned
