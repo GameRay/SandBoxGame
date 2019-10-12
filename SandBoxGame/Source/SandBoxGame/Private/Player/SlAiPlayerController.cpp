d8311431b9cb209141540a1bf448e78c1b670c1c
GameRay <359877454@qq.com>
2019-10-12 16:57:31 +0800

    Controller

--- SandBoxGame/Source/SandBoxGame/Private/Player/SlAiPlayerController.cpp
+++ SandBoxGame/Source/SandBoxGame/Private/Player/SlAiPlayerController.cpp
@@ -3,3 +3,30 @@
 
 #include "SlAiPlayerController.h"
 
+ASlAiPlayerController::ASlAiPlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+void ASlAiPlayerController::Tick(float DeltaSeconds)
+{
+	Super::BeginPlay();
+	if (!SPCharacter)
+	{
+		SPCharacter = Cast<ASlAiPlayerCharacter>(GetCharacter());
+	}
+	bShowMouseCursor = false;
+	FInputModeGameOnly InputMode;
+	InputMode.SetConsumeCaptureMouseDown(true);
+	SetInputMode(InputMode);
+	
+}
+
+void ASlAiPlayerController::BeginPlay()
+{
+}
+
+void ASlAiPlayerController::SetupInputComponent()
+{
+
+	Super::SetupInputComponent();
+}
--- SandBoxGame/Source/SandBoxGame/Public/Player/SlAiPlayerController.h
+++ SandBoxGame/Source/SandBoxGame/Public/Player/SlAiPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "SlAiPlayerCharacter.h"
 #include "SlAiPlayerController.generated.h"
 
 /**
@@ -13,5 +14,15 @@ UCLASS()
 class SANDBOXGAME_API ASlAiPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ASlAiPlayerController();
+	virtual void Tick(float DeltaSeconds)override;
+	
+	
+protected:
+	virtual void BeginPlay()override;
+	virtual void SetupInputComponent()override;
+public:
+	ASlAiPlayerCharacter*SPCharacter;
 	
 };
